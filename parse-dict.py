ea566a8d7ad9d543c144970b75e5ec552780cf94
lizaku55 <lizaku77@gmail.com>
2015-07-05 18:39:23 +0300

    parser-dict improved

--- parse-dict.py
+++ parse-dict.py
@@ -9,44 +9,58 @@ import HTMLParser
 
 hPrs = HTMLParser.HTMLParser()
 
-path_personal = u'/home/lizaku55/PycharmProjects/Thai/'
-path = path_personal + u'letters'
-files = os.listdir(u'letters')
+path_local = u'/home/lizaku55/PycharmProjects/Thai/thai_dict/'
+path_let = path_local + u'letters/'
+files = os.listdir(path_let)
 barr = []
 bdict = {}
 for nomen in files:
-    f = codecs.open(path + nomen, 'r', 'utf-8')
+    f = codecs.open(path_let + nomen, 'r', 'utf-8')
     dtext = f.read()
     dtext = hPrs.unescape(dtext)
-    tokens = re.findall(u"<td class=th><a href='/id/(.+?)'>(.+?)</a></td><td>(.+?)<span class='tt'>(.)</span> (.+?)<span class='tt'>(.)</span> (.+?)<span class='tt'>(.)</span></td><td class=pos>(.+?)</td><td>(.+?)</td>", dtext)
+    entries = re.findall(u"<td class=th>.*?</tr>", dtext)
+    for entry in entries:
+        id = re.findall('<a href=[\'"]([^\'"]*)', re.findall(u'<td[^>]*>(.*?)</td>', entry)[0])[0].split('/')[-1][:6]
+        tokens = [re.sub('<[^>]*>', '', x) for x in re.findall(u'<td[^>]*>(.*?)</td>', entry)]
+        tokens.append(id)
+        barr.append(tokens)
 
-    for i in tokens:
-        arr = []
-        for j in i:
-            j = re.sub(u'<[^>]+>', u' ', j)
-            arr.append(j)
-        #print arr
-        barr.append(arr)
-        #print arr
-for arr in barr:
+    #for i in tokens:
+    #    print i.replace('\r', '').replace('\n', '').replace('\t', '')
+    #    arr = []
+    #    for j in i:
+    #        j = re.sub(u'<[^>]+>', u' ', j)
+    #        arr.append(j)
+    #    #print arr
+    #    barr.append(arr)
+    #    #print arr
+
+for tokens in barr:
     dictic = {}
-    dictic[u'id'] = arr[0]
-    pathic = u'words\\' + arr[0] + u'.html'
-    fword = codecs.open(pathic, u'r', u'utf-8')
-    wtwex = fword.read()
-    wtwex = hPrs.unescape(wtwex)
-    wordtoken = re.findall()
-    dictic[u'transl'] = arr[-1]
-    dictic[u'pos'] = arr[-2]
-    if dictic[u'pos'] == 'example sentence':
-        continue
-    #dictic[u'compounds'] = ' '.join(arr[2:-2])
-    if arr[1] in bdict:
-        bdict[arr[1]].append(dictic)
-    else:
-        bdict[arr[1]] = [dictic]
+    dictic[u'id'] = tokens[4]
+    dictic[u'token'] = tokens[0]
+    dictic[u'transl'] = tokens[3]
+    dictic[u'pos'] = tokens[2]
+    dictic[u'transcr'] = tokens[1]
 
-out = codecs.open(u'outdict.json', u'w', u'utf-8')
+    ## NOT READY YET!
+    #path_word = path_local + u'words/' + tokens[4] + u'.html'
+    #try:
+    #    fword = codecs.open(path_word, u'r', u'utf-8')
+    #    wtwex = fword.read()
+    #    wtwex = hPrs.unescape(wtwex)
+    #    wentries = re.findall(u"<td class=th>.*?</tr>", wtwex)
+        #for wentry in wentries:
+            #wtokens = re.findall(u'<td[^>]*>(.*?)</td>', wentry)
+            #print wtokens
+    #except:
+    #    pass
+    if tokens[0] in bdict:
+        bdict[tokens[0]].append(dictic)
+    else:
+        bdict[tokens[0]] = [dictic]
+
+out = codecs.open(path_local + u'outdict.json', u'w', u'utf-8')
 #js = json.dumps(sorted(bdict), ensure_ascii=False)
 js = json.dump(bdict, out, ensure_ascii=False, indent=2)
 #print js
