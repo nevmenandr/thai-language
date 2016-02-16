5cd4ab5e7f3ed0008f18eb75285c5c739f55ce5e
religofsil <religionofsilence@gmail.com>
2016-02-16 17:00:04 +0300

    Всё работает.

--- parse-dictionary/parsedict.py
+++ parse-dictionary/parsedict.py
@@ -1,4 +1,4 @@
-import lxml.html, os
+import lxml.html, os, codecs
 
 def readdict():
     arrwords=[]
@@ -7,28 +7,32 @@ def readdict():
             f=codecs.open(os.path.join(root2, file), "r", "utf-8")
             f=f.read()
             root=lxml.html.fromstring(f)
-            words=root.xpath('//table[6]/tr')
-            print words[0].text_content()
+            words=root.xpath('//table[@class="gridtable"]/tr')
+            words=words[1:-1]
             for i in words:
                 arrwords.append(i)
     return arrwords
 
+def deletedubs(arr):
+    arr2=[]
+    for i in arr:
+        if i not in arr2:
+            arr2.append(i)
+    return arr2
+
 class Word:
     def __init__(self, root=None):
         try:
-            self.thaiword=root.xpath('/td[@class="th"]')[0].text
+            self.thaiword=root.xpath('./td[@class="th"]')[0].text_content()
         except:
-            self.thaiword='no'
+            self.thaiword='NO'
+        self.pos=root.xpath('./td[@class="pos"]')[0].text_content()
+        if self.thaiword!='NO':
+            self.translit=root.xpath('./td')[1].text_content()
+        else:
+            self.translit='NO'
         try:
-            self.pos=root.xpath('/td[@class=pos]')[0].text
-        except:
-            self.pos='no'
-        try:
-            self.translit=root.xpath('/td')[0][1].text
-        except:
-            self.translit='no'
-        try:
-            self.translation=root.xpath('/td')[0][-1].text
+            self.translation=root.xpath('./td')[-1].text_content()
         except:
             self.translation='no'
 
@@ -36,9 +40,23 @@ def main():
     arrwords=readdict()
     final_arr=[]
     for i in arrwords:
-        x=Word(i)
-        print x.thaiword
-        final_arr.append(x)
+        # print 'word: ', x.thaiword
+        # print 'pos: ', x.pos
+        # print 'translit: ', x.translit
+        # print 'translation: ', x.translation
+        # print '--------------------------------'
+        if i not in final_arr:
+            final_arr.append(Word(i))
+    final_arr=deletedubs(final_arr)
+    for i in final_arr:
+        if i.thaiword=="NO":
+            i.thaiword=final_arr[final_arr.index(i)-1].thaiword
+            i.translit=final_arr[final_arr.index(i)-1].translit
+        print 'word: ', i.thaiword
+        print 'pos: ', i.pos
+        print 'translit: ', i.translit
+        print 'translation: ', i.translation
+        print '--------------------------------'
     return final_arr
 
 main()
