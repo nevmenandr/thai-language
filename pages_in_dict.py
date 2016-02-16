0d1e7d6ef64367301364cb9d71418c68044f658f
NoSurvivalValue <zoot.wowsquirrel@gmail.com>
2016-02-16 14:56:15 +0300

    Update pages_in_dict.py

--- pages_in_dict.py
+++ pages_in_dict.py
@@ -1,15 +1,15 @@
 import codecs
 import os
 import re
+import urllib2
 
 letters = []
-no_letters = []
+one_page_letters = []
 number_of = {}
 existing_pages = {}
 nonexistent_pages = {}
 
-pages = os.listdir("./pages")
-
+pages = os.listdir('./pages')
 
 for page in pages:
     if page.endswith('.html'):
@@ -18,6 +18,7 @@ for page in pages:
         name = u'C:/Users/M/Desktop/Thai/pages/' + page
         f = codecs.open(name, 'r', 'utf-8-sig')
         text = f.read()
+        f.close()
         n = re.findall('Page ([0-9]+?) of ([0-9]+?)\\.', text, flags=re.U)
         number_of[page[0:3]] = n[0][1]
         if page[0:3] in existing_pages:
@@ -25,9 +26,11 @@ for page in pages:
         else:
             existing_pages[page[0:3]] = [n[0][0]]
 
+
 for i in range(161, 206):
     if str(i) not in letters:
-        no_letters.append(str(i))
+        one_page_letters.append(str(i))
+    
 
 for l in letters:
     total = number_of[l]
@@ -41,13 +44,8 @@ for l in letters:
                 else:
                     nonexistent_pages[l] = [str(i)]
 
-print u'There are no pages with these letters:'
-for nl in no_letters:
-    print nl + u' ',
 
-print u''
-print u''
-print u'And the following letters miss the noted pages'
+#print u'And the following pages are not downloaded:'
 
 for np in nonexistent_pages:
     print u''
@@ -55,7 +53,44 @@ for np in nonexistent_pages:
     print u''
     for i in nonexistent_pages[np]:
         print i + u' ',
-    print u''
+#    print u''
+
+exact_urls = []
+
+for np in nonexistent_pages:
+    for p in nonexistent_pages[np]:
+        url = u'http://www.thai-language.com/let/' + np + u'.' + p
+        print url
+        exact_urls.append(url)
+
+starting_urls = []
+
+for nl in no_letters:
+    starting_urls.append(u'http://www.thai-language.com/let/' + nl + u'.2')
+        
+#for eu in exact_urls:
+    #infile = urllib2.urlopen(eu) 
+    #raw_text = infile.read()
+    #text = raw_text.decode("utf-8")
+    #name = u'C:/Users/M/Desktop/Thai/pages/' + eu[33:] + u'.html'
+    #f = codecs.open(name, 'w', 'utf-8-sig')
+    #f.write(text)
+    #f.close()
+
+#for su in starting_urls:
+    #infile = urllib2.urlopen(su) 
+    #raw_text = infile.read()
+    #text = raw_text.decode("utf-8")
+    #name = u'C:/Users/M/Desktop/Thai/pages/' + su[33:] + u'.html'
+    #f = codecs.open(name, 'w', 'utf-8-sig')
+    #f.write(text)
+    #f.close()
+
+
+
+
+
+
 
 
     
