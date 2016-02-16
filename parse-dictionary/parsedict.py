d60abc355c55302a9e90db0493147433506d168b
religofsil <religionofsilence@gmail.com>
2016-02-16 15:26:09 +0300

    Update parsedict.py

--- parse-dictionary/parsedict.py
+++ parse-dictionary/parsedict.py
@@ -2,16 +2,6 @@ import lxml.html, os
 
 def readdict():
     arrwords=[]
-    for root, dirs, files in os.walk('thai_dict/letters'):
-        for file in files:
-            f=file.read()
-            root=lxml.html.fromstring(f)
-            words=root.xpath('//table[@class="gridtable"]/tr')
-            for i in words:
-                arrwords.append(i)import lxml.html, os, codecs
-
-def readdict():
-    arrwords=[]
     for root2, dirs, files in os.walk('thai_dict\\letters'):
         for file in files:
             f=codecs.open(os.path.join(root2, file), "r", "utf-8")
@@ -52,12 +42,3 @@ def main():
     return final_arr
 
 main()
-
-    return arrwords
-
-class Word:
-    def __init__(self, root=None):
-        self.thaiword=root.xpath('/td[@class="tz"]')[0].text
-        self.pos=root.xpath('/td/span[@class="tt"]')[0].text
-        self.translit=root.xpath('/td[@style="white-space:nowrap; text-align:right;"]')[0].text
-        self.translation=root.xpath('/td[@style="vertical-align: top;"]')[0].text
