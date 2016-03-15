6a8eb64611cd5b67e56bb0628042bab3bb347d89
gree-gorey <ignatyeff.g@yandex.ru>
2016-03-15 14:55:54 +0300

    add cleaning text

--- thai_scrapy/thai_scrapy/pipelines.py
+++ thai_scrapy/thai_scrapy/pipelines.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import codecs
+import re
 
 
 class XMLPipeline(object):
 
     def process_item(self, item, spider):
+        item[u'content'] = re.sub(u'(\r\n)+', u' ', item[u'content'], flags=re.U)
+        item[u'content'] = re.sub(u'(\n)+', u' ', item[u'content'], flags=re.U)
+        item[u'content'] = re.sub(u' +', u' ', item[u'content'], flags=re.U)
         with codecs.open(u'./output/' + str(item[u'name']) + u'.xml', u'w', u'utf-8') as f:
             text = u'<?xml version="1.0" encoding="UTF-8"?>\n<xml>\n' \
                    u'<meta>\n<link>' + item[u'link'] + u'</link>\n' +\
