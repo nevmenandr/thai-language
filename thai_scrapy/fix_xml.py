d3e8e925f38a56f448b9fe8f0ed6205bb9ddcb20
gree-gorey <ignatyeff.g@yandex.ru>
2016-03-22 21:32:15 +0300

    fix pipeline

--- thai_scrapy/fix_xml.py
+++ thai_scrapy/fix_xml.py
@@ -4,25 +4,30 @@ import time
 import codecs
 import os
 
-__author__ = 'Gree-gorey'
+__author__ = 'gree-gorey'
 
 
-def texts(path):
+def texts(path, new_path):
     for root, dirs, files in os.walk(path):
         for filename in files:
             open_name = path + filename
             with codecs.open(open_name, u'r', u'utf-8') as f:
                 lines = f.readlines()
-                lines.insert(1, u'<xml>\n')
-                lines.insert(10, u'\n</xml>')
-            with codecs.open(open_name, u'w', u'utf-8') as w:
+                try:
+                    if lines[1] != u'<xml>\n':
+                        lines.insert(1, u'<xml>\n')
+                        lines.insert(10, u'\n</xml>')
+                except:
+                    pass
+            write_name = new_path + filename
+            with codecs.open(write_name, u'w', u'utf-8') as w:
                 w.write(u''.join(lines))
 
 
 def main():
     t1 = time.time()
 
-    texts(u'./dailynews.co.th/')
+    texts(u'./to_fix/', u'./new/')
 
     t2 = time.time()
     print t2 - t1
--- thai_scrapy/thai_scrapy/pipelines.py
+++ thai_scrapy/thai_scrapy/pipelines.py
@@ -15,6 +15,11 @@ class XMLPipeline(object):
         item[u'content'] = re.sub(u'\.+', u'.', item[u'content'], flags=re.U)
         item[u'content'] = re.sub(u'\t+', u'', item[u'content'], flags=re.U)
 
+        item[u'title'] = re.sub(u'(\r\n)+', u' ', item[u'title'], flags=re.U)
+        item[u'title'] = re.sub(u'(\n)+', u' ', item[u'title'], flags=re.U)
+        item[u'title'] = re.sub(u' +', u' ', item[u'title'], flags=re.U)
+        item[u'title'] = re.sub(u'\t+', u'', item[u'title'], flags=re.U)
+
         with codecs.open(u'./thaigov.go.th/' + str(item[u'name']) + u'.xml', u'w', u'utf-8') as f:
             text = u'<?xml version="1.0" encoding="UTF-8"?>\n<xml>\n' \
                    u'<meta>\n<link>' + unquote(item[u'link']).decode(u'utf-8') + u'</link>\n' +\
