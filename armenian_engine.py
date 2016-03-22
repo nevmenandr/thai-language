6cb8564ec07d580d24d846588f0f47230ee7bce5
gree-gorey <ignatyeff.g@yandex.ru>
2016-03-22 13:49:22 +0300

    fix prs conversion

--- armenian_engine.py
+++ armenian_engine.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 
+import os
+import re
 import time
 import codecs
-import os
 from lxml import etree
 
 __author__ = 'gree-gorey'
@@ -10,22 +11,48 @@ __author__ = 'gree-gorey'
 
 class PrsItem:
     def __init__(self):
-        self.head = u'#sentno\t#wordno\t#lang\t#graph\t#word\t#indexword\t#nvars\t#nlems\t#nvar\t#lem\t#trans\t' \
-                    u'#trans_ru\t#lex\t#gram\t#flex\t#punctl\t#punctr\t#sent_pos\n'
         self.docid = None
-        self.author = None
         self.title = None
-        self.date1 = None
-        self.date2 = None
         self.genre = None
         self.words = None
         self.sentences = None
-        self.date_displayed = None
-        self.data = None
+        self.data = []
 
     def get_text(self):
-        result = self.head + self.sentences
+        head = u'#sentno\t#wordno\t#lang\t#graph\t#word\t#indexword\t#nvars\t#nlems\t#nvar\t#lem\t#trans\t' \
+               u'#trans_ru\t#lex\t#gram\t#flex\t#punctl\t#punctr\t#sent_pos\n'
+        docid = u'#meta.docid\t' + str(self.docid) + u'\n'
+        author = u'#meta.author\t\n'
+        title = u'#meta.title\t' + self.title + u'\n'
+        date1 = u'#meta.date1\t\n'
+        date2 = u'#meta.date2\t\n'
+        genre = u'#meta.genre\t' + self.genre + u'\n'
+        words = u'#meta.words\t' + str(self.words) + u'\n'
+        sentences = u'#meta.sentences\t' + str(self.sentences) + u'\n'
+        date_displayed = u'#meta.date_displayed\t\n'
+        body = u''.join(self.data)
+        result = head + docid + author + title + date1 + date2 + genre + words + sentences + date_displayed + body
         return result
+
+
+class Word:
+    def __init__(self):
+        self.content = None
+        self.pos = None
+        self.sentence_number = None
+        self.word_number = None
+        self.nvar = None
+        self.nlems = None
+        self.self_number = None
+        self.lemma = None
+        self.translation = None
+        self.pos = None
+        self.sent = u''
+
+    def get_result(self):
+        return str(self.sentence_number) + u'\t' + str(self.word_number) + u'\t\t\t' + self.content + u'\t\t' +\
+               str(self.nvar) + u'\t' + str(self.nlems) + u'\t' + str(self.self_number) + u'\t' + self.lemma + u'\t\t' +\
+               self.translation + u'\t' + self.pos + u'\t' + self.pos + u'\t\t\t\t' + self.sent + u'\n'
 
 
 def read_xml(path):
@@ -38,14 +65,42 @@ def read_xml(path):
 
 
 def write_prs(tree, text_id, path):
-    new_prs = PrsItem()
+    prs = PrsItem()
+    prs.docid = text_id
+    prs.title = tree.xpath('//meta/title')[0].text
+    prs.genre = tree.xpath('//meta/genre')[0].text
     sentences = tree.xpath('//body/se')
-    new_prs.sentences = u'#meta.sentences\t' + str(len(sentences)) + u'\n'
-    for i, sentence in enumerate(sentences):
-        pass
-        # print i, sentence
+    prs.sentences = len(sentences)
+    for i, sentence in enumerate(sentences, start=1):
+        words = sentence.xpath('./w')
+        for j, word in enumerate(words, start=1):
+            '''
+            В этом месте нужно поменять все на просто word.text, когда будет человеческий XML
+            # new_word.content = word.text
+            '''
+            content = u''.join([x for x in word.itertext()])
+            content = re.sub(u' +', u'', content, re.U)
+            content = re.sub(u'\r\n+', u'', content, re.U)
+            content = re.sub(u'\n+', u'', content, re.U)
+            analyses = word.xpath('./ana')
+            nvar = len(analyses)
+            lemmata = len(set([analysis.get('lex') for analysis in analyses]))
+            for k, analysis in enumerate(analyses, start=1):
+                new_word = Word()
+                new_word.content = content
+                new_word.sentence_number = i
+                new_word.word_number = j
+                new_word.self_number = k
+                new_word.nvar = nvar
+                new_word.nlems = lemmata
+                new_word.lemma = analysis.get(u'lex')
+                new_word.translation = analysis.get(u'trans')
+                new_word.pos = analysis.get(u'gr')
 
-    prs_result = new_prs.get_text()
+                word_result = new_word.get_result()
+                prs.data.append(word_result)
+
+    prs_result = prs.get_text()
     write_name = path + u'text_id_' + str(text_id) + u'.prs'
     with codecs.open(write_name, u'w', u'utf-8') as w:
         w.write(prs_result)
--- thai_scrapy/thai_scrapy/pipelines.py
+++ thai_scrapy/thai_scrapy/pipelines.py
@@ -2,18 +2,20 @@
 
 import codecs
 import re
+from urllib2 import unquote
 
 
 class XMLPipeline(object):
 
     def process_item(self, item, spider):
+        item[u'content'] = re.sub(u' +', u' ', item[u'content'], flags=re.U)
         item[u'content'] = re.sub(u'(\r\n)+', u' ', item[u'content'], flags=re.U)
         item[u'content'] = re.sub(u'(\n)+', u' ', item[u'content'], flags=re.U)
         item[u'content'] = re.sub(u' +', u' ', item[u'content'], flags=re.U)
         item[u'content'] = re.sub(u'\.+', u'.', item[u'content'], flags=re.U)
         with codecs.open(u'./thaipoem.com/' + str(item[u'name']) + u'.xml', u'w', u'utf-8') as f:
             text = u'<?xml version="1.0" encoding="UTF-8"?>\n<xml>\n' \
-                   u'<meta>\n<link>' + item[u'link'] + u'</link>\n' +\
+                   u'<meta>\n<link>' + unquote(item[u'link']).decode(u'utf-8') + u'</link>\n' +\
                    u'<title>' + item[u'title'] + u'</title>\n<genre>poem</genre>\n</meta>\n<text>\n' + \
                    item[u'content'] + u'\n</text>\n</xml>'
             f.write(text)
